d1661b0b0dfe1ec5011a97a56533b090d9cec483
smschijf <210578@student.glu.nl>
2023-07-18 14:10:02 +0200

    example added of xss

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,55 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navigation from './Navigation';
-import Jumbotron from './Jumbotron';
-import Feed from './Feed';
-import Contact from './Contact';
-import About from './About';
-import data from '../data/data.json';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navigation from "./Navigation";
+import Jumbotron from "./Jumbotron";
+import Feed from "./Feed";
+import Contact from "./Contact";
+import About from "./About";
+import data from "../data/data.json";
+import "./App.css";
+
+const createMarkup = () => {
+  return { _html: "dangerous markup" };
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       name: "Manny Henri",
       jumbotronTitle: "List of courses",
       feeds: [],
-    }
+    };
   }
 
   UNSAFE_componentWillMount() {
     this.setState({
       feeds: data,
-    })
+    });
   }
   render() {
     return (
       <Router>
         <div className="container">
           <Navigation />
-          <Jumbotron title={this.state.jumbotronTitle}/>
+          <Jumbotron title={this.state.jumbotronTitle} />
           <Switch>
-            <Route path="/contact" component={Contact}/>
-            <Route path="/about" component={About}/>
-            <Route exact path="/" render={(props) => (
-              <Feed feeds={this.state.feeds} />
-            )} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/about" component={About} />
+            <Route
+              exact
+              path="/"
+              render={(props) => <Feed feeds={this.state.feeds} />}
+            />
           </Switch>
           <div className="footer">
-                <p>&copy; {this.state.name} Inc.</p>
+            <p>&copy; {this.state.name} Inc.</p>
+            <div innerHTML={createMarkup()}></div>
+            <div dangerouslySetInnerHTML={createMarkup()}></div>
           </div>
         </div>
       </Router>
-    )
+    );
   }
 }
 
