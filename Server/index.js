255f35e19bc7f0f1e3d1e54d757a8a104155a247
smschijf <210578@student.glu.nl>
2023-07-21 13:32:25 +0200

    created API endpoint

--- Server/index.js
+++ Server/index.js
@@ -3,12 +3,17 @@ import jwt from "express-jwt";
 import cors from "cors";
 import jwks from "jwks-rsa";
 import bodyParser from "body-parser";
+import courses from "./courses.json" assert {type: "json"};
 
 const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(port, () => {
